5664c8e08d430ce2ea6d9e0ec3102414105e37ca
sevonj <100710152+sevonj@users.noreply.github.com>
2025-03-23 15:50:13 +0200

    fix: router getting stuck

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import UserHeader from '@/components/headers/UserHeader.vue'
-import type { GetPersonDetails, LemmyHttp } from 'lemmy-js-client'
-import { getCurrentInstance } from 'vue'
+import type { GetPersonDetails, GetPersonDetailsResponse, LemmyHttp } from 'lemmy-js-client'
+import { getCurrentInstance, ref } from 'vue'
 import { useRoute } from 'vue-router'
 
 const route = useRoute()
@@ -13,12 +13,19 @@ const client: LemmyHttp = instance?.appContext.config.globalProperties.$client
 const getPersonForm: GetPersonDetails = {
   username: identifier,
 }
-const user = await client.getPersonDetails(getPersonForm)
+
+const user = ref<GetPersonDetailsResponse | undefined>()
+
+async function fetchUser(): Promise<void> {
+  user.value = await client.getPersonDetails(getPersonForm)
+}
+
+fetchUser()
 </script>
 
 <template>
   <main>
-    <UserHeader :user />
+    <UserHeader v-if="user" :user />
   </main>
 
   <div style="min-height: 100vh"></div>
