846d9406372791fef3cd7ee38a684796ff3588b2
sevonj <100710152+sevonj@users.noreply.github.com>
2025-03-23 22:14:47 +0200

    Refactor: extract feed header

--- src/views/CommunityView.vue
+++ src/views/CommunityView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
 import FeedThe, { type FeedLocation } from '@/components/FeedThe.vue'
+import FeedNav from '@/components/FeedNav.vue'
+import CommunityHeader from '@/components/headers/CommunityHeader.vue'
 import { communityRequestIdentifier } from '@/lib/actors'
 import type { GetCommunity, LemmyHttp } from 'lemmy-js-client'
 import { getCurrentInstance, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
+import { toRef } from 'vue'
 
 const route = useRoute()
 const instance = getCurrentInstance()
@@ -36,7 +39,18 @@ fetchFeedLocation()
 </script>
 
 <template>
-  <FeedThe v-if="feedLocation" :feed-location />
+  <div v-if="feedLocation">
+    <div class="feed-header">
+      <FeedNav :location="toRef(feedLocation)" />
+      <CommunityHeader v-if="feedLocation.v == 'Community'" :comm="feedLocation.data" />
+    </div>
+    <FeedThe :feed-location />
+  </div>
 </template>
 
-<style></style>
+<style>
+.feed-header {
+  padding: 0 8px 12px 8px;
+  background: var(--color-background-soft);
+}
+</style>
