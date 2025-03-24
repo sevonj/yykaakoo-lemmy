c4859985ec103975dd2a0e71a71bcf447793d67f
sevonj <100710152+sevonj@users.noreply.github.com>
2025-03-24 14:50:46 +0200

    voteblock

--- src/components/CommentThe.vue
+++ src/components/CommentThe.vue
@@ -4,8 +4,8 @@ import { ref, getCurrentInstance, type Ref } from 'vue'
 import VueMarkdown from 'vue-markdown-render'
 import SpeechBubble from './common/SpeechBubble.vue'
 import UserMeta from './common/UserMeta.vue'
+import VoteBlock from './common/VoteBlock.vue'
 import Badge from './common/BadgeThe.vue'
-import { ArrowUpIcon, ArrowDownIcon } from '@heroicons/vue/24/solid'
 import RelativeTimestamp from './textformat/RelativeTimestamp.vue'
 
 const instance = getCurrentInstance()
@@ -45,15 +45,10 @@ async function getReplies() {
 
       <template v-slot:below>
         <div class="flex-row flex-1" style="gap: 12px">
-          <div class="flex-row no-shrink" style="">
-            <ArrowUpIcon class="meta-icon" />
-            <p>{{ commentView.counts.upvotes }}</p>
-          </div>
-
-          <div class="flex-row no-shrink" style="">
-            <ArrowDownIcon class="meta-icon" />
-            <p>{{ commentView.counts.downvotes }}</p>
-          </div>
+          <VoteBlock
+            :upvotes="commentView.counts.upvotes"
+            :downvotes="commentView.counts.downvotes"
+          />
         </div>
         <RelativeTimestamp :timestamp="commentView.counts.published" />
       </template>
@@ -68,7 +63,7 @@ async function getReplies() {
     </UserMeta>
 
     <div class="comment-reply-cont" v-if="commentView.counts.child_count">
-      <a v-if="replies.length == 0" @click="getReplies">
+      <a v-if="replies.length == 0" @click="getReplies" class="button-link">
         Fetch replies ({{ commentView.counts.child_count }})
       </a>
       <CommentThe v-for="reply in replies" :comment-view="reply" :key="reply.comment.id" />
